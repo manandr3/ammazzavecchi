b422fa8e0ea23003d21710bd9c3a85d8dc9affcf
mana320228 <84560447+manandr3@users.noreply.github.com>
2025-08-24 22:47:22 +0200

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -12,6 +12,9 @@ public partial class Player : CharacterBody3D
 	public int JumpImpulse { get; set; } = 20;
 	
 	private Vector3 _targetVelocity = Vector3.Zero;
+	private Vector3 camera_offset = Vector3.Zero;
+	private int max_camera_zoomout = 12;
+	private int max_camera_zoomin = -2;
 	public override void _PhysicsProcess(double delta)
 	{
 		var direction = Vector3.Zero;
@@ -28,17 +31,13 @@ public partial class Player : CharacterBody3D
 		}
 		if (Input.IsActionPressed("move_back"))
 		{
-			
 			direction.Z += 1.0f;
 			direction.X += 1.0f;
-			//direction.Z += 1.0f;
 		}
 		if (Input.IsActionPressed("move_forward"))
 		{
 			direction.X -= 1.0f;
 			direction.Z -= 1.0f;
-			
-			//direction.Z -= 1.0f;
 		}
 
 		if (direction != Vector3.Zero)
@@ -63,12 +62,38 @@ public partial class Player : CharacterBody3D
 			_targetVelocity.Y = JumpImpulse;
 		}
 		
-		
 		MoveAndSlide();
 		
 		//camera movement
 		var CameraController = GetNode<Node3D>("Camera_Controller");
-		CameraController.GlobalPosition = Lerp3(CameraController.GlobalPosition,GlobalPosition,0.05f);
+		CameraController.GlobalPosition = Lerp3(CameraController.GlobalPosition,GlobalPosition + camera_offset, 0.01f*((max_camera_zoomout - max_camera_zoomin)-(camera_offset.X)));
+	}
+	
+	//mouse wheel controller to modify camera position
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		{
+			switch (mouseEvent.ButtonIndex)
+			{
+				case MouseButton.WheelUp:
+					if(camera_offset.X > max_camera_zoomin)
+					{
+						camera_offset.X -= 1f;
+						camera_offset.Y -= 1f;
+						camera_offset.Z -= 1f;
+					}
+				   	break;
+				case MouseButton.WheelDown:
+					if(camera_offset.X < max_camera_zoomout)
+					{
+						camera_offset.X += 1f;
+						camera_offset.Y += 1f;
+						camera_offset.Z += 1f;
+					}
+					break;
+			}
+		}
 	}
 	
 	public static Vector3 Lerp3(Vector3 First, Vector3 Second, float Amount)
