c67ba8836c5346741d613a2b927d3f9d9e5eecf4
mana320228 <84560447+manandr3@users.noreply.github.com>
2025-09-18 19:37:21 +0200

    adaptive camera distance
    finally added this cool new camera feature that allows to navigate ambientations with ostacles that obstruct the camera view of the player without resulting in a continue back and forth of the same.

--- AmbienteProva.cs
+++ AmbienteProva.cs
@@ -1,24 +1,18 @@
 using Godot;
 using System;
 
-public partial class Main : Node
+public partial class AmbienteProva : Node
 {
-	[Export] public DirectionalLight3D DirectionalLight;
+
+    [Export] public DirectionalLight3D DirectionalLight;
 	[Export] public MeshInstance3D TargetMesh; // where the shader is applied
 	[Export] private Shader toonShader; // the shader reference that applies the toon effect to all the meshes
 
 	private Vector3 light_rotation = Vector3.Zero;
 	private DirectionalLight3D sunlight;
 
-	//update light direction for shader usage
-	public override void _Process(double delta)
-	{
-		//making tha sun slowly rotating
-		light_rotation.Y += 0.00002f;
-		sunlight.Rotation = light_rotation;
-	}
 
-	public override void _Ready()
+    public override void _Ready()
 	{
 
 		var palette = new Godot.Collections.Array<Color>()
@@ -101,66 +95,10 @@ public partial class Main : Node
 
 		SetProcess(true);
 
-		ConvertAllMeshes(GetTree().Root);
+		//ConvertAllMeshes(GetTree().Root);
 		
 		light_rotation.X = -30 * (MathF.PI/180);
 		light_rotation.Y = -23 * (MathF.PI/180);
 		light_rotation.Y = 32 * (MathF.PI/180);
 	}
-
-	public override void _PhysicsProcess(double delta)
-	{
-		if (DirectionalLight == null || TargetMesh == null)
-			return;
-
-		// Ottieni la direzione della luce (asse -Z del transform)
-		Vector3 dir = -DirectionalLight.GlobalTransform.Basis.Z.Normalized();
-
-		// Aggiorna il parametro nello shader
-		var mat = TargetMesh.GetActiveMaterial(0) as ShaderMaterial;
-		if (mat != null)
-		{
-			mat.SetShaderParameter("light_direction", dir);
-		}
-
-
-
-
-		
-	}
-
-	public override void _Input(InputEvent @event)
-	{
-		if (Input.IsActionPressed("menu"))
-			Input.MouseMode = Input.MouseModeEnum.Visible;
-	}
-	
-	private void ConvertAllMeshes(Node node)
-    {
-        // Se il nodo è una MeshInstance3D, convertilo
-        if (node is MeshInstance3D mesh)
-        {
-			if (mesh.Mesh != null)
-			{
-				var originalMaterial = mesh.GetSurfaceOverrideMaterial(0);
-				if (originalMaterial is StandardMaterial3D standardMat)
-				{
-
-					// Crea il nuovo ShaderMaterial
-					ShaderMaterial shaderMat = new ShaderMaterial();
-					shaderMat.Shader = toonShader;
-					shaderMat.SetShaderParameter("light_direction", -DirectionalLight.GlobalTransform.Basis.Z.Normalized());
-
-					// Applica il nuovo materiale
-					//mesh.SetSurfaceOverrideMaterial(0, shaderMat);
-				}
-			}
-        }
-
-        // Scansiona ricorsivamente tutti i figli
-        foreach (Node child in node.GetChildren())
-        {
-            ConvertAllMeshes(child);
-        }
-    }
 }
--- CameraController.cs
+++ CameraController.cs
@@ -11,38 +11,71 @@ public partial class CameraController : Node3D
 	private Vector3 camera_offset = new Vector3(0, 4, 4);
 	private int max_camera_zoomout = 12;
 	private int max_camera_zoomin = 1;
-	private Vector3 first_person_offset = new Vector3(0, 0.5f, 0);
+	private Vector3 first_person_offset = new Vector3(0, 1.0f, 0);
 	
 	private float _yaw = 0f;
 	private float _pitch = 0f;
 	
-	private static SpringArm3D SpringArm;
-	private static Node3D Camera_Target;
+	private static SpringArm3D SpringArmGround;
+	private static SpringArm3D SpringArmObjects;
+	private static Node3D CameraTarget;
+	private static Node3D CameraTargetCollision;
 	private static Camera3D Camera;
-	
+	private static Area3D CameraCollisionsChecker;
+	private bool CameraCollided = false;
+
 	public override void _Ready()
 	{
-		SpringArm = GetNode<SpringArm3D>("Spring_Arm");
-		Camera_Target = GetNode<Node3D>("Spring_Arm/Camera_Target");
+		SpringArmGround = GetNode<SpringArm3D>("Spring_Arm_Ground");
+		SpringArmObjects = GetNode<SpringArm3D>("Spring_Arm_Objects");
+		CameraTarget = GetNode<Node3D>("Spring_Arm_Ground/Camera_Target");
+		CameraTargetCollision = GetNode<Node3D>("Spring_Arm_Objects/Camera_Target_Collision");
 		Camera = GetNode<Camera3D>("Camera3D");
+		CameraCollisionsChecker = GetNode<Area3D>("CameraCollisionsChecker");
 	}
 	
 	public override void _PhysicsProcess(double delta)
 	{
 		var Player = GetParent() as CharacterBody3D;
+
+		if (CameraCollided && CameraTarget.Position == CameraTargetCollision.Position)
+			CameraCollided = false;
 		
-		
-		SpringArm.SpringLength = Lerp(SpringArm.SpringLength, camera_offset.Z, 0.2f);
-		if(camera_offset.Z == max_camera_zoomin)
+		SpringArmObjects.SpringLength = Lerp(SpringArmObjects.SpringLength, camera_offset.Z, 0.2f);
+		SpringArmGround.SpringLength = Lerp(SpringArmGround.SpringLength, camera_offset.Z, 0.2f);
+
+		if (CameraCollided)
 		{
-			Camera.Position = new Vector3(0, 0, 0);
-			GlobalPosition = Player.GlobalPosition + first_person_offset;
+			SpringArmObjects.SpringLength = Lerp(SpringArmObjects.SpringLength, camera_offset.Z, 0.2f);
+			if (camera_offset.Z == max_camera_zoomin)
+			{
+				Camera.Position = new Vector3(0, 0, 0);
+				GlobalPosition = Player.GlobalPosition + first_person_offset;
+			}
+			else
+			{
+				Camera.Position = Lerp3(Camera.Position, CameraTargetCollision.Position, 0.2f);
+				CameraCollisionsChecker.Position = Lerp3(Camera.Position, CameraTargetCollision.Position, 0.2f);
+				GlobalPosition = Lerp3(GlobalPosition, Player.GlobalPosition, 0.2f);
+			}
+			//Camera.Position = Lerp3(Camera.Position, CameraTargetCollision.Position, 0.2f);
 		}
 		else
 		{
-			Camera.Position = Lerp3(Camera.Position, Camera_Target.Position, 0.2f);
-			GlobalPosition = Lerp3(GlobalPosition, Player.GlobalPosition, 0.2f);
+			if (camera_offset.Z == max_camera_zoomin)
+			{
+				Camera.Position = new Vector3(0, 0, 0);
+				GlobalPosition = Player.GlobalPosition + first_person_offset;
+			}
+			else
+			{
+				Camera.Position = Lerp3(Camera.Position, CameraTarget.Position, 0.2f);
+				CameraCollisionsChecker.Position = Lerp3(Camera.Position, CameraTarget.Position, 0.2f);
+				GlobalPosition = Lerp3(GlobalPosition, Player.GlobalPosition, 0.2f);
+			}
 		}
+		
+		
 		
 		Rotation = new Vector3
 		(
@@ -51,40 +84,47 @@ public partial class CameraController : Node3D
 			0
 		);
 	}
-	
+
 	public override void _UnhandledInput(InputEvent @event)
 	{
-		if(Input.MouseMode == Input.MouseModeEnum.Captured)
+		if (Input.MouseMode == Input.MouseModeEnum.Captured)
 		{
-			if(@event is InputEventMouseMotion eventMouseMotion)
+			if (@event is InputEventMouseMotion eventMouseMotion)
 			{
 				_yaw -= eventMouseMotion.Relative.X * camera_sensitivity;
 				_pitch = Mathf.Clamp(_pitch - eventMouseMotion.Relative.Y * camera_sensitivity, MinPitch, MaxPitch);
 			}
-			
+
 			if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
 			{
-				if(Input.MouseMode == Input.MouseModeEnum.Captured)
+				if (Input.MouseMode == Input.MouseModeEnum.Captured)
 				{
 					switch (mouseEvent.ButtonIndex)
 					{
 						case MouseButton.WheelUp:
-							if(camera_offset.Z != max_camera_zoomin)
+							if (camera_offset.Z != max_camera_zoomin)
 							{
 								camera_offset.Z -= 1f;
 							}
-						break;
-							
+							break;
+
 						case MouseButton.WheelDown:
-							if(camera_offset.Z != max_camera_zoomout)
+							if (camera_offset.Z != max_camera_zoomout)
 							{
 								camera_offset.Z += 1f;
 							}
-						break;
+							break;
 					}
 				}
 			}
 		}
+	}
+
+	private void OnCameraCollisionsCheckerAreaShapeEntered(Node3D body)
+	{
+		CameraCollided = true;
+		Camera.Position = CameraTargetCollision.Position;
+		
 	}
 	
 	public static Vector3 Lerp3(Vector3 First, Vector3 Second, float Amount)
